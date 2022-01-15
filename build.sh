30334a5bf0c8251ab5c7179e2e5452955309860a
Pig Fang <g-plane@hotmail.com>
2022-01-15 16:39:41 +0800

    make build faster

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ sed '/^CFLAGS/s/$/ -s INVOKE_RUN=0 -s EXIT_RUNTIME=0 -s EXPORTED_RUNTIME_METHODS
 sed '/^CFLAGS/s/$/ -s INVOKE_RUN=0 -s EXIT_RUNTIME=0 -s EXPORTED_RUNTIME_METHODS="[FS, callMain]" -s MODULARIZE=1 -s EXPORT_ES6=1&/' \
   -i src/metaflac/Makefile
 emmake make clean
-emmake make
+emmake make -j
 cp src/flac/.libs/flac ../wasm/flac/flac.js
 cp src/flac/.libs/flac.wasm ../wasm/flac/flac.wasm
 cp src/metaflac/.libs/metaflac ../wasm/metaflac/metaflac.js
