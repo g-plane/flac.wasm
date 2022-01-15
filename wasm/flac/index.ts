3d08fb06837dfbe3fafefa3afee85fba09f1775b
Pig Fang <g-plane@hotmail.com>
2022-01-15 15:28:25 +0800

    enhance interaction

--- wasm/flac/index.ts
+++ wasm/flac/index.ts
@@ -2,20 +2,53 @@
 import loadModule from './flac.js'
 
 export interface Options {
-  inputFileName: string
-  inputFile: Uint8Array
-  outputFileName: string
+  inputFileName?: string
+  inputFile?: Uint8Array
+  outputFileName?: string
+}
+
+export interface Output {
+  exitCode: number
+  stdout: string
+  stderr: string
+  file: Uint8Array | undefined
 }
 
 export async function flac(
   args: string[],
-  options: Options
-): Promise<Uint8Array> {
-  const { FS, callMain } = await loadModule()
+  { inputFileName, inputFile, outputFileName }: Options
+): Promise<Output> {
+  let stdout = ''
+  let stderr = ''
+  const { FS, callMain } = await loadModule({
+    print(text: string) {
+      stdout += text + '\n'
+    },
+    printErr(text: string) {
+      stderr += text + '\n'
+    },
+  })
 
-  FS.writeFile(options.inputFileName, options.inputFile)
+  if (inputFile && inputFileName) {
+    FS.writeFile(inputFileName, inputFile)
+  }
 
-  callMain(args)
+  const exitCode = callMain(args)
 
-  return FS.readFile(options.outputFileName)
+  if (!outputFileName) {
+    return {
+      exitCode,
+      stdout,
+      stderr,
+      file: undefined,
+    }
+  }
+
+  const { exists } = FS.analyzePath(outputFileName)
+  return {
+    exitCode,
+    stdout,
+    stderr,
+    file: exists ? FS.readFile(outputFileName) : undefined,
+  }
 }
