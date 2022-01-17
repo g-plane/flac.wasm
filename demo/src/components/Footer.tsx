3c4d7f53e5a878fb5363232b716f408772fe574a
Pig Fang <g-plane@hotmail.com>
2022-01-17 11:21:39 +0800

    fix layout of footer

--- demo/src/components/Footer.tsx
+++ demo/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class="text-center py-4 absolute bottom-0 w-full bg-white">
+    <footer class="text-center py-4 sticky bottom-0 w-full bg-white">
       <a href="https://github.com/g-plane/flac.wasm" target="_blank" rel="noopener noreferrer">
         GitHub
       </a>{' '}
