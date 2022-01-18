b9044fd9b23cbae77edcfd55f39afd6352127465
Pig Fang <g-plane@hotmail.com>
2022-01-18 09:56:40 +0800

    refactor(flac): simplify build config

--- wasm/flac/rollup.config.ts
+++ wasm/flac/rollup.config.ts
@@ -1,20 +1,14 @@
 import * as path from 'node:path'
 import * as process from 'node:process'
 import { defineConfig } from 'rollup'
-import swc from 'rollup-plugin-swc3'
 
 export default defineConfig({
-  input: './index.ts',
+  input: './index.js', // the source TypeScript code was built by tsc
   output: {
     file: './workerized.js',
     format: 'iife',
   },
   plugins: [
-    swc({
-      jsc: {
-        target: 'es2018',
-      },
-    }),
     {
       name: 'replace-import-meta-url',
       resolveImportMeta(prop, { moduleId }) {
