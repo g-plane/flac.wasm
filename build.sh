76a767ae9424569e11d6b18ef871f26549e80052
Pig Fang <g-plane@hotmail.com>
2022-01-20 09:24:47 +0800

    build: simplify build and prepare for emscripten v3.1.1

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -xe
 
 cd flac
 ./autogen.sh
-emconfigure ./configure CFLAGS=-Oz
+emconfigure ./configure --disable-examples --disable-option-checking --disable-cpplibs CFLAGS=-Oz
 sed '/^CFLAGS/s/$/ -s INVOKE_RUN=0 -s EXIT_RUNTIME=0 -s EXPORTED_RUNTIME_METHODS="[FS, callMain]" -s MODULARIZE=1 -s EXPORT_ES6=1 -s ENVIRONMENT=worker&/' \
   -i src/flac/Makefile
 sed '/^CFLAGS/s/$/ -s INVOKE_RUN=0 -s EXIT_RUNTIME=0 -s EXPORTED_RUNTIME_METHODS="[FS, callMain]" -s MODULARIZE=1 -s EXPORT_ES6=1 -s ENVIRONMENT=worker&/' \
