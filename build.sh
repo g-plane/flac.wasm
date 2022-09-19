e1227cea94ff43f56d61cc72971c3e54c861c2e2
Pig Fang <g-plane@hotmail.com>
2022-09-19 23:44:10 +0800

    suppress emscripten error

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -xe
 cd flac
 ./autogen.sh
 emconfigure ./configure --disable-examples --disable-option-checking --disable-cpplibs CFLAGS=-Oz
-sed '/^CFLAGS/s/$/ -sINVOKE_RUN=0 -sEXIT_RUNTIME=0 -sEXPORTED_RUNTIME_METHODS="[FS, callMain]" -sMODULARIZE=1 -sEXPORT_ES6=1 -sENVIRONMENT=worker&/' \
+sed '/^CFLAGS/s/$/ -sINVOKE_RUN=0 -sEXIT_RUNTIME=0 -sEXPORTED_RUNTIME_METHODS="[FS, callMain]" -sMODULARIZE=1 -sEXPORT_ES6=1 -sENVIRONMENT=worker -sERROR_ON_UNDEFINED_SYMBOLS=0&/' \
   -i src/flac/Makefile
-sed '/^CFLAGS/s/$/ -sINVOKE_RUN=0 -sEXIT_RUNTIME=0 -sEXPORTED_RUNTIME_METHODS="[FS, callMain]" -sMODULARIZE=1 -sEXPORT_ES6=1 -sENVIRONMENT=worker&/' \
+sed '/^CFLAGS/s/$/ -sINVOKE_RUN=0 -sEXIT_RUNTIME=0 -sEXPORTED_RUNTIME_METHODS="[FS, callMain]" -sMODULARIZE=1 -sEXPORT_ES6=1 -sENVIRONMENT=worker -sERROR_ON_UNDEFINED_SYMBOLS=0&/' \
   -i src/metaflac/Makefile
 emmake make clean
 emmake make -j
